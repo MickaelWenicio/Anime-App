160c587cdbe8011f8a5e9e74dc90a4e5971aa0b0
Mickael Wenicio <mickael.pogramador123@gmail.com>
2024-03-19 07:16:02 -0300

    feat: search input added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
 import {Container} from './Style'
 import TopChars from '../../components/TopCharacters/TopChars.jsx';
+import magnifying from '../../../public/magnifying-glass.png'
 
 const Home = () => {
   
@@ -9,14 +9,14 @@ const Home = () => {
     <Container>
         <div id='homeContent'>
           <div className='trending'>
-            <div>
+            <div className='trendingHeader'>
               <div className='trendingTitle'>
-                <h2>Em Alta</h2>
-                
+                <h2>EM ALTA</h2>
               </div>
-              {/* <form>
-                  <input type="text" placeholder='Pesquisar'/>
-              </form> */}
+              <form method='GET'>
+                  <input type='search' placeholder='Pesquisar'/>
+                  <button><img src={magnifying}/></button>
+              </form>
             </div>
             
             <div className='trendingDescription'>
@@ -25,7 +25,7 @@ const Home = () => {
           </div>
           <div id='trendingCharacters'>
             <div className='trendingCategoryTitle'>
-              <h2>Top Personagens</h2>
+              <h3>Top Personagens</h3>
               <div className='divisor'></div>
             </div>
             <TopChars/>
--- src/pages/home/Style.js
+++ src/pages/home/Style.js
@@ -13,11 +13,52 @@ export const Container = styled.div`
 
     .trending{
         width: 100%;
+        .trendingHeader{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            input[type=search]{
+                background-color: #3B3B3B;
+                width: 15vw;
+                height: 5vh;
+                padding-left:1.5em;
+                border-radius: 10em;
+                border: none;
+                outline: none;
+                font-size: 1.1rem;
+                color: #ffffff;
+            }
+
+            input::placeholder {
+                color: var(--text-gray);
+            }
+
+
+            }
+
+            form{
+                display: flex;
+                align-items: center;
+                position: relative;
+                button{
+                    background:none;
+                    border: none;
+                    img{
+                        width: 1.2vw;
+                    }
+                    position: absolute;
+                    right: 1em;
+                    cursor: pointer;
+                }
+            }
+    }
 
         .trendingTitle{
             border-left: solid .2em red;
             padding: .4em 0 .4em;
             h2{
+                font-weight: 800;
                 letter-spacing: 1px;
                 padding-left: .5em;
                 font-size: 1.8em;
@@ -31,13 +72,13 @@ export const Container = styled.div`
                 font-weight: 500;
             }
         }
-    }
 
     .trendingCategoryTitle{
-        margin-top: 4vh;
+        margin-top: 6vh;
         margin-bottom: 2vh;
-        h2{
+        h3{
             font-weight: 500;
+            font-size: 1.5em;
         }
 
         .divisor{
@@ -47,4 +88,6 @@ export const Container = styled.div`
             background-color:red;
         }
     }
+
+    
 `
